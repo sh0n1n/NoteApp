438b38a26cb1154f326fe43df2833700ff214b3d
Artem <dogmaster31072003@gmail.com>
2025-07-16 14:09:51 +0300

    Extension+Color

---
+++ NoteApp/Extensions/UIColor+Ex.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+extension UIColor {
+    static let lighWhite = UIColor(red: 233/255, green: 238/255, blue: 242/255, alpha: 1)
+    static let lightBlack =  UIColor(red: 15/255, green: 16/255, blue: 16/255, alpha: 1)
+        
+}
