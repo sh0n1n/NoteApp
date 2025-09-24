06de8e11765f1fe8b5b9f71bbc207bcb43ef7a27
Artem <dogmaster31072003@gmail.com>
2025-09-24 12:25:36 +0300

    created enumfor categories

--- NoteApp/NotesList/Model/Note.swift
+++ NoteApp/NotesList/Model/Note.swift
@@ -1,9 +1,17 @@
 import UIKit
 
+enum NoteCategory: String, CaseIterable {
+    case personal
+    case work
+    case shopping
+    case ideas
+    case other
+}
 struct Note: TableViewItemProtocol {
     let title: String
     let description: String
     let date: Date
     let imageUrl: String?
     let image: Data?
+    let category: NoteCategory
 }
