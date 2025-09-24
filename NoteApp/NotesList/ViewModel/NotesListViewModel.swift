463c46e4d86d600f62d3e44bcccabdb97ef90f2f
Artem <dogmaster31072003@gmail.com>
2025-09-24 12:28:37 +0300

    in setMocks added our new parameter

--- NoteApp/NotesList/ViewModel/NotesListViewModel.swift
+++ NoteApp/NotesList/ViewModel/NotesListViewModel.swift
@@ -17,8 +17,8 @@ final class NotesListViewModel: NotesListViewModelProtocol {
     }
     
     private func setMocks() {
-        let section = TableViewSection(title: "16 Jul 2025", items: [Note(title: "First title", description: "1st description", date: Date(), imageUrl: nil, image: nil),
-                                                                     Note(title: "Second title", description: "2nd description", date: Date(), imageUrl: nil, image: nil)])
+        let section = TableViewSection(title: "16 Jul 2025", items: [Note(title: "First title", description: "1st description", date: Date(), imageUrl: nil, image: nil, category: .personal),
+                                                                     Note(title: "Second title", description: "2nd description", date: Date(), imageUrl: nil, image: nil, category: .ideas)])
         
         self.section = [section]
     }
