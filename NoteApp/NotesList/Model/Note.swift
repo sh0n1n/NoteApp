3b3472b29527a341cda38d0fceeb1a74c6f714ac
Artem <dogmaster31072003@gmail.com>
2025-09-24 12:53:45 +0300

    Added switch for categories and their colors

--- NoteApp/NotesList/Model/Note.swift
+++ NoteApp/NotesList/Model/Note.swift
@@ -6,6 +6,26 @@ enum NoteCategory: String, CaseIterable {
     case shopping
     case ideas
     case other
+    
+    var switchImageName: String {
+        switch self {
+        case .personal: return "personal"
+        case .work: return "work"
+        case .shopping: return "shopping"
+        case .ideas: return "ideas"
+        case .other: return "other"
+        }
+    }
+    
+    var color: UIColor {
+        switch self {
+        case .personal: return .systemBlue
+        case .work: return .systemOrange
+        case .shopping: return .systemTeal
+        case .ideas: return .systemGray
+        case .other: return .systemPink
+        }
+    }
 }
 struct Note: TableViewItemProtocol {
     let title: String
